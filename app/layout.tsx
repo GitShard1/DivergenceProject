dd3f33cac89c5fa0b35a0099b0101f869292d1ea
Joseph Charles Dasilva Barbosa <josephbarbosa416@gmail.com>
2026-01-17 16:48:51 -0500

    fixed import error

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import { Provider } from "@/components/ui/provider"
-import { ChakraProvider } from "@chakra-ui/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
