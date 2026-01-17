3ef239b7ed06f69c5cf92b617c9d62a9e345a68f
Joseph Charles Dasilva Barbosa <josephbarbosa416@gmail.com>
2026-01-17 03:45:27 -0500

    Added mongo connection

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,31 @@
+import os
 from fastapi import FastAPI
 from pymongo import MongoClient
 from pydantic import BaseModel
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
+
+from pymongo.mongo_client import MongoClient
+
+MONGO_USERNAME = os.getenv("MONGO_USERNAME")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+uri = F"mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}@cluster0.ncuqrpz.mongodb.net/?appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
 # MongoDB connection (replace with your connection string)
-client = MongoClient("mongodb://localhost:27017/")
 db = client["mydatabase"]
 collection = db["items"]
 
